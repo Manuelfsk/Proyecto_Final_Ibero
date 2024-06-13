938db505674cac2c5cd1f01d67f6b0be8e0cd4cc
Manuel Fonseca Avila <fonsecafernando88@gmail.com>
2024-06-13 17:39:35 -0500

    eliminacion de los modulos carrusel y colleges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { Carrusel1Component } from './components/carrusel1/carrusel1.component';
 import { BodyhomeComponent } from './components/bodyhome/bodyhome.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
-import { CollagesComponent } from './components/collages/collages.component';
 import { ContactusComponent } from './components/contactus/contactus.component';
 import { LoginComponent } from './components/login/login.component';
 import { LocationComponent } from './components/location/location.component';
@@ -17,31 +15,37 @@ import { ShopComponent } from './components/shop/shop.component';
 import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductosComponent } from './components/productos/productos.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    Carrusel1Component,
     BodyhomeComponent,
     FooterComponent,
     GalleryComponent,
-    CollagesComponent,
     ContactusComponent,
     LoginComponent,
     LocationComponent,
     ShopComponent,
     DashboardComponent,
     MenuComponent,
+    ProductosComponent,
+    CategoriasComponent,
+    UsuariosComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    
+    HttpClientModule,   
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
