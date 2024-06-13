24163fcee0b5f6491ef64ed2604e0407cbb7733e
Manuel Fonseca Avila <fonsecafernando88@gmail.com>
2024-06-13 17:40:19 -0500

    creacion de los componentes categorias productos usuarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BodyhomeComponent } from './components/bodyhome/bodyhome.component';
-import { CollagesComponent } from './components/collages/collages.component';
+//import { CollagesComponent } from './components/collages/collages.component';
 import { ContactusComponent } from './components/contactus/contactus.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { LocationComponent } from './components/location/location.component';
 import { LoginComponent } from './components/login/login.component';
 import { ShopComponent } from './components/shop/shop.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
 
 const routes: Routes = [
@@ -15,10 +18,10 @@ const routes: Routes = [
   //A. rutas header y footer
   // { path: '/navbar', component: NavbarComponent, pathMatch: "full" },
   // { path: '/footer', component: FooterComponent, pathMatch: "full" },
-  
+
 
   //1. ruta home
-  { path: "",  component: BodyhomeComponent, pathMatch: "full" },
+  { path: "", component: BodyhomeComponent, pathMatch: "full" },
   //2.ruta gallery
   { path: "gallery", component: GalleryComponent, pathMatch: "full" },
   //3.ruta shop
@@ -31,8 +34,14 @@ const routes: Routes = [
   { path: "location", component: LocationComponent, pathMatch: "full" },
   // 7. ruta dashboard
   { path: "dashboard", component: DashboardComponent, pathMatch: "full" },
+  // 8. ruta categorias
+  { path: "categorias", component: CategoriasComponent, pathMatch: "full" },
+  // 9. ruta productos
+  { path: "productos", component: ProductosComponent, pathMatch: "full" },
+  // 10. ruta usuarios
+  { path: "usuarios", component: UsuariosComponent, pathMatch: "full" },
 
-  ];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
