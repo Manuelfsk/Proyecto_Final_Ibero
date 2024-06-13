19d7cf53141d2bc6c32f04e2698705ac6cf84685
Manuel Fonseca Avila <fonsecafernando88@gmail.com>
2024-06-13 17:41:27 -0500

    creando funcion logear() para email y password

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeticionService } from 'src/app/servicios/peticion.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
+  constructor(private peticion:PeticionService){
+    
+  }
+
+  emailogin:string = ""
+  password:string = ""
+  datos:any[] =[]
+
+  logear(){
+    console.log(this.emailogin)
+    console.log(this.password)
+
+    this.datos.push({emailogin: this.emailogin, password:this.password})
+    console.log(this.datos)
+  }
+
+
 
 }
