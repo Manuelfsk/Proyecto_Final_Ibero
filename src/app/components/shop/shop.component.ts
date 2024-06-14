2053951ef571fe33d88b89cb9ddefa1a1cc13509
Manuel Fonseca Avila <fonsecafernando88@gmail.com>
2024-06-14 17:31:26 -0500

    creacion del shop con estilo de bootstrap cards falta cuadrar la imagen que debe salir pre determinada y listo

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeticionService } from 'src/app/servicios/peticion.service';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.css']
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit {
+  ngOnInit(): void {
+    this.CargarTdoas()
+  }
 
+  constructor(private peticion: PeticionService) { }
+  imagen: string = ""
+  nombre: string = ""
+  precio: number = 0
+  datos: any[] = []
+
+  CargarTdoas() {
+    let post = {
+      Host: this.peticion.urlhost,
+      path: "/productos/read",
+      payload: {
+
+      }
+
+    }
+    this.peticion.Post(post.Host + post.path, post.payload).then(
+      (res: any) => {
+        console.log(res)
+        this.datos = res.respuesta.data
+
+      }
+    )
+  }
 }
