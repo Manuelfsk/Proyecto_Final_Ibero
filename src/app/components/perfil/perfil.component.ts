34a5f3d1d990ff29aa8f76d4eb3bfa4577f3c6b4
Manuel Fonseca Avila <fonsecafernando88@gmail.com>
2024-06-30 14:10:58 -0500

    se ajusta css de perfil y el usuario puede cambiar de password, falta ahcer la actualizacion de datos y bloquear elementos para que los usuarios no lo realicen

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PeticionService } from 'src/app/servicios/peticion.service';
 
+declare var Swal: any
+
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
@@ -16,8 +18,8 @@ export class PerfilComponent implements OnInit {
   apellidos: string = ""
   email: string = ""
   password: string = ""
-  nuevo_password: string = ""
   telefono: number = 0
+  confirmarpass: string = ""
 
   Cargar() {
     let post = {
@@ -28,16 +30,56 @@ export class PerfilComponent implements OnInit {
 
       }
     }
-  this.peticion.Post(post.Host + post.path, post.payload).then(
+    this.peticion.Post(post.Host + post.path, post.payload).then(
       (res: any) => {
         console.log(res)
-          this.nombre = res.respuesta.data[0].nombre,
+        this.nombre = res.respuesta.data[0].nombre,
           this.apellidos = res.respuesta.data[0].apellidos,
           this.email = res.respuesta.data[0].email,
           this.password = res.respuesta.data[0].password,
-          this.nuevo_password = res.respuesta.data[0].nuevo_password,
           this.telefono = res.respuesta.data[0].telefono
       }
     )
   }
+
+  ActualizarPass() {
+    if (this.password != this.confirmarpass) {
+      Swal.fire({
+        icon: "error",
+        title: "Se presentó un problema",
+        text: "Su password no es igual en los campos seleccionados"
+      });
+    } else {
+      let post = {
+        Host: this.peticion.urlhost,
+        path: "/usuarios/actualizarpass",
+        payload: {
+          password: this.password
+        }
+      }
+      this.peticion.Post(post.Host + post.path, post.payload).then(
+        (res: any) => {
+          console.log(res)
+          if (res.state == true) {
+            Swal.fire({
+              icon: "success",
+              title: "perfecto",
+              text: res.mensaje,
+            });
+          } else {
+            Swal.fire({
+              icon: "error",
+              title: "ouchh!!!1",
+              text: res.mensaje,
+            });
+          }
+        }
+      )
+
+
+    }
+  }
+
+
+
 }
