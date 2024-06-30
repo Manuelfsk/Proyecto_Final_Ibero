6391909ad106c10cf87f36616d3dd4e0492f5be0
Manuel Fonseca Avila <fonsecafernando88@gmail.com>
2024-06-30 11:58:22 -0500

    ajustes de perfil para capturar datos y modificar los datos de usuarios

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeticionService } from 'src/app/servicios/peticion.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
 
+  constructor(private peticion: PeticionService) { }
+  ngOnInit(): void {
+    this.Cargar()
+  }
+  nombre: string = ""
+  apellidos: string = ""
+  email: string = ""
+  password: string = ""
+  nuevo_password: string = ""
+  telefono: number = 0
+
+  Cargar() {
+    let post = {
+      Host: this.peticion.urlhost,
+      path: "/usuarios/perfil",
+      payload: {
+
+
+      }
+    }
+  this.peticion.Post(post.Host + post.path, post.payload).then(
+      (res: any) => {
+        console.log(res)
+          this.nombre = res.respuesta.data[0].nombre,
+          this.apellidos = res.respuesta.data[0].apellidos,
+          this.email = res.respuesta.data[0].email,
+          this.password = res.respuesta.data[0].password,
+          this.nuevo_password = res.respuesta.data[0].nuevo_password,
+          this.telefono = res.respuesta.data[0].telefono
+      }
+    )
+  }
 }
